8f33c76e94bbb87a3bc4a6b7096f538d0603e653
mhendric <mhendric@microsoft.com>
2015-07-10 11:07:32 -0700

    Adding end to end example used in my Ignite 2015 presentation

---
+++ Examples/EndToEndExample/9-ConfigureLCMForContinuousChecking.ps1
@@ -0,0 +1,24 @@
+Configuration ConfigureLCMForContinuousChecking
+{
+    Node $AllNodes.NodeName
+    {
+        LocalConfigurationManager
+        {
+            CertificateId                  = $Node.Thumbprint
+            RebootNodeIfNeeded             = $false
+            ConfigurationMode              = "ApplyAndAutoCorrect"
+            ConfigurationModeFrequencyMins = 30
+        }
+    }
+}
+
+if ($RemoteServerCreds -eq $null)
+{
+    $RemoteServerCreds = Get-Credential -Message "Enter credentials for connecting to remote Exchange Server"
+}
+
+###Compiles the example
+ConfigureLCMForContinuousChecking -ConfigurationData $PSScriptRoot\ExchangeSettings-Lab.psd1
+
+###Sets up LCM on target computers to decrypt credentials, and to allow reboot during resource execution
+#Set-DscLocalConfigurationManager -Path .\ConfigureLCMForContinuousChecking -Verbose -ComputerName XXX
