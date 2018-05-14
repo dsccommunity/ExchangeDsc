ed25213251784230de087be809a20952ea8a7905
Jean-Cyril Drouhin <jdrouhin@microsoft.com>
2018-05-14 21:38:29 +0200

    Add ConfigurationData.psd1 file

--- Examples/MaintenanceMode/MaintenanceModePrep.ps1
+++ Examples/MaintenanceMode/MaintenanceModePrep.ps1
@@ -3,38 +3,7 @@
     This example shows how to prepare maintenance mode.
 #>
 
-$ConfigurationData = @{
-    AllNodes = @(
-        @{
-            NodeName        = '*'
-
-            <#
-                NOTE! THIS IS NOT RECOMMENDED IN PRODUCTION.
-                This is added so that AppVeyor automatic tests can pass, otherwise
-                the tests will fail on passwords being in plain text and not being
-                encrypted. Because it is not possible to have a certificate in
-                AppVeyor to encrypt the passwords we need to add the parameter
-                'PSDscAllowPlainTextPassword'.
-                NOTE! THIS IS NOT RECOMMENDED IN PRODUCTION.
-                See:
-                http://blogs.msdn.com/b/powershell/archive/2014/01/31/want-to-secure-credentials-in-windows-powershell-desired-state-configuration.aspx
-            #>
-            PSDscAllowPlainTextPassword = $true
-
-            #Thumbprint of the certificate being used for decrypting credentials
-            Thumbprint      = '49930c16428c9e5fdc8461d551eae19d9eb3670c'
-
-            Site1DC         = 'dc-1'
-            Site2DC         = 'dc-2'
-        }
-
-        #Individual target nodes are defined next
-        @{
-            NodeName = 'e15-1'
-            NodeFqdn = 'e15-1.mikelab.local'
-        }
-    )
-}
+$ConfigurationData = Import-PowerShellDataFile -Path (Join-Path -Path $PSScriptRoot -ChildPath 'ConfigurationData.psd1')
 
 Configuration Example
 {
--- Examples/MaintenanceMode/MaintenanceModeStart.ps1
+++ Examples/MaintenanceMode/MaintenanceModeStart.ps1
@@ -3,35 +3,7 @@
     This example shows how to start maintenance mode.
 #>
 
-$ConfigurationData = @{
-    AllNodes = @(
-        @{
-            NodeName        = '*'
-
-            <#
-                NOTE! THIS IS NOT RECOMMENDED IN PRODUCTION.
-                This is added so that AppVeyor automatic tests can pass, otherwise
-                the tests will fail on passwords being in plain text and not being
-                encrypted. Because it is not possible to have a certificate in
-                AppVeyor to encrypt the passwords we need to add the parameter
-                'PSDscAllowPlainTextPassword'.
-                NOTE! THIS IS NOT RECOMMENDED IN PRODUCTION.
-                See:
-                http://blogs.msdn.com/b/powershell/archive/2014/01/31/want-to-secure-credentials-in-windows-powershell-desired-state-configuration.aspx
-            #>
-            PSDscAllowPlainTextPassword = $true
-
-            Site1DC         = 'dc-1'
-            Site2DC         = 'dc-2'
-        }
-
-        #Individual target nodes are defined next
-        @{
-            NodeName = 'e15-1'
-            NodeFqdn = 'e15-1.mikelab.local'
-        }
-    )
-}
+$ConfigurationData = Import-PowerShellDataFile -Path (Join-Path -Path $PSScriptRoot -ChildPath 'ConfigurationData.psd1')
 
 Configuration Example
 {
