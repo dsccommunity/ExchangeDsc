e5dec2458ba5d1a3634f3b3dc0d79608cdf2d1f9
Jean-Cyril Drouhin <jdrouhin@microsoft.com>
2018-05-16 10:05:42 +0200

    Revert to $ConfigurationData in each example files

--- Examples/MaintenanceMode/MaintenanceModePrep.ps1
+++ Examples/MaintenanceMode/MaintenanceModePrep.ps1
@@ -3,8 +3,45 @@
     This example shows how to prepare maintenance mode.
 #>
 
-$ConfigurationDataFile = Join-Path -Path $PSScriptRoot -ChildPath 'ConfigurationData.psm1'
-. $ConfigurationDataFile
+$ConfigurationData = @{
+    AllNodes = @(
+        @{
+            #region Common Settings for All Nodes
+            NodeName        = '*'
+
+            <#
+                NOTE! THIS IS NOT RECOMMENDED IN PRODUCTION.
+                This is added so that AppVeyor automatic tests can pass, otherwise
+                the tests will fail on passwords being in plain text and not being
+                encrypted. Because it is not possible to have a certificate in
+                AppVeyor to encrypt the passwords we need to add the parameter
+                'PSDscAllowPlainTextPassword'.
+                NOTE! THIS IS NOT RECOMMENDED IN PRODUCTION.
+                See:
+                http://blogs.msdn.com/b/powershell/archive/2014/01/31/want-to-secure-credentials-in-windows-powershell-desired-state-configuration.aspx
+            #>
+            PSDscAllowPlainTextPassword = $true
+
+            <#
+                The location of the exported public certifcate which will be used to encrypt
+                credentials during compilation.
+                CertificateFile = 'C:\public-certificate.cer' 
+            #>
+            
+            #Thumbprint of the certificate being used for decrypting credentials
+            Thumbprint      = '39bef4b2e82599233154465323ebf96a12b60673' 
+
+            Site1DC         = 'dc-1'
+            Site2DC         = 'dc-2'
+        }
+
+        #Individual target nodes are defined next
+        @{
+            NodeName = 'e15-1'
+            NodeFqdn = 'e15-1.mikelab.local'
+        }
+    )
+}
 
 Configuration Example
 {
