be3f4f132035b41cea068aa4800ceee6a348a858
Ariel Moisis <armoisis@microsoft.com>
2018-12-12 15:33:08 +1100

    Update xExchUMService with all params

--- DSCResources/MSFT_xExchUMService/MSFT_xExchUMService.psm1
+++ DSCResources/MSFT_xExchUMService/MSFT_xExchUMService.psm1
@@ -24,6 +24,52 @@ function Get-TargetResource
         $DialPlans,
 
         [Parameter()]
+        [System.String[]]
+        $GrammarGenerationSchedule,
+
+        [Parameter()]
+        [ValidateSet('IPv4Only', 'IPv6Only', 'Any')]
+        [System.String]
+        $IPAddressFamily,
+
+        [Parameter()]
+        [System.Boolean]
+        $IPAddressFamilyConfigurable,
+
+        [Parameter()]
+        [System.Boolean]
+        $IrmLogEnabled,
+
+        [Parameter()]
+        [System.String]
+        $IrmLogMaxAge,
+
+        [Parameter()]
+        [System.String]
+        $IrmLogMaxDirectorySize,
+
+        [Parameter()]
+        [System.String]
+        $IrmLogMaxFileSize,
+
+        [Parameter()]
+        [System.String]
+        $IrmLogPath,
+
+        [Parameter()]
+        [System.Int32]
+        $MaxCallsAllowed,
+
+        [Parameter()]
+        [System.String]
+        $SIPAccessService,
+
+        [Parameter()]
+        [ValidateSet('Enabled', 'Disabled', 'NoNewCalls')]
+        [System.String]
+        $Status,
+
+        [Parameter()]
         [System.String]
         $DomainController
     )
@@ -45,6 +91,17 @@ function Get-TargetResource
             Identity      = [System.String] $Identity
             UMStartupMode = [System.String] $umService.UMStartupMode
             DialPlans     = [System.String[]] $umService.DialPlans
+            GrammarGenerationSchedule = [System.String[]] $umService.GrammarGenerationSchedule
+            IPAddressFamily = [System.String] $umService.IPAddressFamily
+            IPAddressFamilyConfigurable = [System.String] $umService.IPAddressFamilyConfigurable
+            IrmLogEnabled = [System.String] $umService.IrmLogEnabled
+            IrmLogMaxAge = [System.String] $umService.IrmLogMaxAge
+            IrmLogMaxDirectorySize  = [System.String] $umService.IrmLogMaxDirectorySize
+            IrmLogMaxFileSize = [System.String] $umService.IrmLogMaxFileSize
+            IrmLogPath = [System.String] $umService.IrmLogPath
+            MaxCallsAllowed  = [System.String] $umService.MaxCallsAllowed
+            SIPAccessService = [System.String] $umService.SIPAccessService
+            Status = [System.String] $umService.Status
         }
     }
 
@@ -74,6 +131,52 @@ function Set-TargetResource
         [Parameter()]
         [System.String[]]
         $DialPlans,
+
+        [Parameter()]
+        [System.String[]]
+        $GrammarGenerationSchedule,
+
+        [Parameter()]
+        [ValidateSet('IPv4Only', 'IPv6Only', 'Any')]
+        [System.String]
+        $IPAddressFamily,
+
+        [Parameter()]
+        [System.Boolean]
+        $IPAddressFamilyConfigurable,
+
+        [Parameter()]
+        [System.Boolean]
+        $IrmLogEnabled,
+
+        [Parameter()]
+        [System.String]
+        $IrmLogMaxAge,
+
+        [Parameter()]
+        [System.String]
+        $IrmLogMaxDirectorySize,
+
+        [Parameter()]
+        [System.String]
+        $IrmLogMaxFileSize,
+
+        [Parameter()]
+        [System.String]
+        $IrmLogPath,
+
+        [Parameter()]
+        [System.Int32]
+        $MaxCallsAllowed,
+
+        [Parameter()]
+        [System.String]
+        $SIPAccessService,
+
+        [Parameter()]
+        [ValidateSet('Enabled', 'Disabled', 'NoNewCalls')]
+        [System.String]
+        $Status,
 
         [Parameter()]
         [System.String]
@@ -118,6 +221,52 @@ function Test-TargetResource
         $DialPlans,
 
         [Parameter()]
+        [System.String[]]
+        $GrammarGenerationSchedule,
+
+        [Parameter()]
+        [ValidateSet('IPv4Only', 'IPv6Only', 'Any')]
+        [System.String]
+        $IPAddressFamily,
+
+        [Parameter()]
+        [System.Boolean]
+        $IPAddressFamilyConfigurable,
+
+        [Parameter()]
+        [System.Boolean]
+        $IrmLogEnabled,
+
+        [Parameter()]
+        [System.String]
+        $IrmLogMaxAge,
+
+        [Parameter()]
+        [System.String]
+        $IrmLogMaxDirectorySize,
+
+        [Parameter()]
+        [System.String]
+        $IrmLogMaxFileSize,
+
+        [Parameter()]
+        [System.String]
+        $IrmLogPath,
+
+        [Parameter()]
+        [System.Int32]
+        $MaxCallsAllowed,
+
+        [Parameter()]
+        [System.String]
+        $SIPAccessService,
+
+        [Parameter()]
+        [ValidateSet('Enabled', 'Disabled', 'NoNewCalls')]
+        [System.String]
+        $Status,
+
+        [Parameter()]
         [System.String]
         $DomainController
     )
@@ -143,6 +292,50 @@ function Test-TargetResource
             $testResults = $false
         }
         if (!(Test-ExchangeSetting -Name 'DialPlans' -Type 'Array' -ExpectedValue $DialPlans -ActualValue $umService.DialPlans -PSBoundParametersIn $PSBoundParameters -Verbose:$VerbosePreference))
+        {
+            $testResults = $false
+        }
+        if (!(Test-ExchangeSetting -Name 'GrammarGenerationSchedule' -Type 'Array' -ExpectedValue $GrammarGenerationSchedule -ActualValue $umService.GrammarGenerationSchedule -PSBoundParametersIn $PSBoundParameters -Verbose:$VerbosePreference))
+        {
+            $testResults = $false
+        }
+        if (!(Test-ExchangeSetting -Name 'IPAddressFamily' -Type 'IPAddressFamily' -ExpectedValue $IPAddressFamily -ActualValue $umService.IPAddressFamily -PSBoundParametersIn $PSBoundParameters -Verbose:$VerbosePreference))
+        {
+            $testResults = $false
+        }
+        if (!(Test-ExchangeSetting -Name 'IPAddressFamilyConfigurable' -Type 'Boolean' -ExpectedValue $IPAddressFamilyConfigurable -ActualValue $umService.IPAddressFamilyConfigurable -PSBoundParametersIn $PSBoundParameters -Verbose:$VerbosePreference))
+        {
+            $testResults = $false
+        }
+        if (!(Test-ExchangeSetting -Name 'IrmLogEnabled' -Type 'Boolean' -ExpectedValue $IrmLogEnabled -ActualValue $umService.IrmLogEnabled -PSBoundParametersIn $PSBoundParameters -Verbose:$VerbosePreference))
+        {
+            $testResults = $false
+        }
+        if (!(Test-ExchangeSetting -Name 'IrmLogMaxAge' -Type 'Timespan' -ExpectedValue $IrmLogMaxAge -ActualValue $umService.IrmLogMaxAge -PSBoundParametersIn $PSBoundParameters -Verbose:$VerbosePreference))
+        {
+            $testResults = $false
+        }
+        if (!(Test-ExchangeSetting -Name 'IrmLogMaxDirectorySize' -Type 'ByteQuantifiedSize' -ExpectedValue $IrmLogMaxDirectorySize -ActualValue $umService.IrmLogMaxDirectorySize -PSBoundParametersIn $PSBoundParameters -Verbose:$VerbosePreference))
+        {
+            $testResults = $false
+        }
+        if (!(Test-ExchangeSetting -Name 'IrmLogMaxFileSize' -Type 'ByteQuantifiedSize' -ExpectedValue $IrmLogMaxFileSize -ActualValue $umService.IrmLogMaxFileSize -PSBoundParametersIn $PSBoundParameters -Verbose:$VerbosePreference))
+        {
+            $testResults = $false
+        }
+        if (!(Test-ExchangeSetting -Name 'IrmLogPath' -Type 'LocalLongFullPath' -ExpectedValue $IrmLogPath -ActualValue $umService.IrmLogPath -PSBoundParametersIn $PSBoundParameters -Verbose:$VerbosePreference))
+        {
+            $testResults = $false
+        }
+        if (!(Test-ExchangeSetting -Name 'MaxCallsAllowed' -Type 'Int' -ExpectedValue $MaxCallsAllowed -ActualValue $umService.MaxCallsAllowed -PSBoundParametersIn $PSBoundParameters -Verbose:$VerbosePreference))
+        {
+            $testResults = $false
+        }
+        if (!(Test-ExchangeSetting -Name 'SIPAccessService' -Type 'ProtocolConnectionSettings' -ExpectedValue $SIPAccessService -ActualValue $umService.SIPAccessService -PSBoundParametersIn $PSBoundParameters -Verbose:$VerbosePreference))
+        {
+            $testResults = $false
+        }
+        if (!(Test-ExchangeSetting -Name 'Status' -Type 'ServerStatus' -ExpectedValue $Status -ActualValue $umService.Status -PSBoundParametersIn $PSBoundParameters -Verbose:$VerbosePreference))
         {
             $testResults = $false
         }
