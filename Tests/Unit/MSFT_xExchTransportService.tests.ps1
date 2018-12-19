2197e50d8d44325278275e15731c4d8d300f71b9
Ariel Moisis <armoisis@microsoft.com>
2018-12-19 20:52:23 +1100

    update test to go into catch block

--- Tests/Unit/MSFT_xExchTransportService.tests.ps1
+++ Tests/Unit/MSFT_xExchTransportService.tests.ps1
@@ -224,6 +224,16 @@ try
                     Set-TargetResource @setTargetResourceParams
                 }
 
+                It 'Should return a null with a terminating error on Set-TransportService' {
+                    $setTargetResourceParams.PipelineTracingSenderAddress = $null
+                    Mock -CommandName Write-FunctionEntry -Verifiable
+                    Mock -CommandName Assert-IsSupportedWithExchangeVersion -Verifiable
+                    Mock -CommandName Get-RemoteExchangeSession -Verifiable
+                    Mock -CommandName Set-TransportService -Verifiable -MockWith {throw 'Transport Service Error'}
+
+                    Set-TargetResource @setTargetResourceParams
+                }
+
                 It 'Should return a warning when AllowServiceRestart is false' {
                     $setTargetResourceParams.AllowServiceRestart = $false
                     Mock -CommandName Write-FunctionEntry -Verifiable
