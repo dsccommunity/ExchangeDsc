79492d53225b392efa6c69a9ed0eee41c672b7e5
Ariel Moisis <armoisis@microsoft.com>
2018-12-21 09:24:24 +1100

    fix up after rebase

--- Tests/Unit/MSFT_xExchTransportService.tests.ps1
+++ Tests/Unit/MSFT_xExchTransportService.tests.ps1
@@ -225,16 +225,6 @@ try
                     Set-TargetResource @setTargetResourceParams
                 }
 
-                It 'Should return a null with a terminating error on Set-TransportService' {
-                    $setTargetResourceParams.PipelineTracingSenderAddress = $null
-                    Mock -CommandName Write-FunctionEntry -Verifiable
-                    Mock -CommandName Assert-IsSupportedWithExchangeVersion -Verifiable
-                    Mock -CommandName Get-RemoteExchangeSession -Verifiable
-                    Mock -CommandName Set-TransportService -Verifiable -MockWith {throw 'Transport Service Error'}
-
-                    Set-TargetResource @setTargetResourceParams
-                }
-
                 It 'Should return a warning when AllowServiceRestart is false' {
                     $setTargetResourceParams.AllowServiceRestart = $false
                     Mock -CommandName Write-FunctionEntry -Verifiable
@@ -285,7 +275,6 @@ try
 
                     Test-TargetResource @commonTargetResourceParams | Should -Be $true
                 }
-
             }
         }
     }
