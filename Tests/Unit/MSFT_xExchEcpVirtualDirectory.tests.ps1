d11b1568ac15e7d3e71914e77e7a48cbbec891c9
Ariel Moisis <armoisis@microsoft.com>
2019-01-09 11:17:18 +1100

    update missing parameters and unit tests - need to fix throw check

--- Tests/Unit/MSFT_xExchEcpVirtualDirectory.tests.ps1
+++ Tests/Unit/MSFT_xExchEcpVirtualDirectory.tests.ps1
@@ -52,12 +52,16 @@ try
                 AdfsAuthentication            = [System.Boolean] $false
                 BasicAuthentication           = [System.Boolean] $false
                 DigestAuthentication          = [System.Boolean] $false
+                ExtendedProtectionFlags         = [System.String[]] @()
+                ExtendedProtectionSPNList       = [System.String[]] @()
+                ExtendedProtectionTokenChecking = [System.String] ''
                 ExternalAuthenticationMethods = [System.String[]] @()
                 ExternalUrl                   = [System.String] ''
                 FormsAuthentication           = [System.Boolean] $false
                 GzipLevel                     = [System.String] ''
                 InternalUrl                   = [System.String] ''
                 WindowsAuthentication         = [System.Boolean] $false
+                OwaOptionsEnabled               = [System.Boolean] $false
             }
 
             Context 'When Get-TargetResource is called' {
@@ -66,6 +70,121 @@ try
                 Mock -CommandName Get-EcpVirtualDirectoryInternal -Verifiable -MockWith { return $getEcpVirtualDirectoryStandardOutput }
 
                 Test-CommonGetTargetResourceFunctionality -GetTargetResourceParams $getTargetResourceParams
+            }
+        }
+
+        Describe 'MSFT_xExchEcpVirtualDirectory\Set-TargetResource' -Tag 'Set' {
+            AfterEach {
+                Assert-VerifiableMock
+            }
+
+            $setTargetResourceParams = @{
+                Identity   = 'EcpVirtualDirectory'
+                Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'fakeuser', (New-Object -TypeName System.Security.SecureString)
+            }
+
+            $setEcpVirtualDirectoryStandardOutput = @{
+                AdminEnabled                  = [System.Boolean] $false
+                AdfsAuthentication            = [System.Boolean] $false
+                BasicAuthentication           = [System.Boolean] $false
+                DigestAuthentication          = [System.Boolean] $false
+                ExtendedProtectionFlags         = [System.String[]] @()
+                ExtendedProtectionSPNList       = [System.String[]] @()
+                ExtendedProtectionTokenChecking = [System.String] ''
+                ExternalAuthenticationMethods = [System.String[]] @()
+                ExternalUrl                   = [System.String] ''
+                FormsAuthentication           = [System.Boolean] $false
+                GzipLevel                     = [System.String] ''
+                InternalUrl                   = [System.String] ''
+                WindowsAuthentication         = [System.Boolean] $false
+                OwaOptionsEnabled               = [System.Boolean] $false
+            }
+
+            Context 'When Set-TargetResource is called' {
+                It 'Should warn about restarting the MSExchangeECPAppPool' {
+                    function Set-ECPVirtualDirectory {
+                    }
+                    Mock -CommandName Write-FunctionEntry -Verifiable
+                    Mock -CommandName Get-RemoteExchangeSession -Verifiable
+                    Mock -CommandName Set-ECPVirtualDirectory -Verifiable
+                    Mock -CommandName Write-Warning -ParameterFilter {$message -eq 'The configuration will not take effect until MSExchangeECPAppPool is manually recycled.'}
+                    Set-TargetResource @setTargetResourceParams
+                }
+
+                It 'Should call expected functions' {
+                    function Set-ECPVirtualDirectory {
+                    }
+                    $setTargetResourceParams.AllowServiceRestart = $true
+                    Mock -CommandName Write-FunctionEntry -Verifiable
+                    Mock -CommandName Get-RemoteExchangeSession -Verifiable
+                    Mock -CommandName Set-ECPVirtualDirectory -Verifiable
+                    Mock -CommandName Restart-ExistingAppPool -Verifiable
+                    Set-TargetResource @setTargetResourceParams
+                    $setTargetResourceParams.Remove('AllowServiceReset')
+                }
+
+                It 'Should throw error about SPN' {
+                    Mock -CommandName Write-FunctionEntry -Verifiable
+                    Mock -CommandName Get-RemoteExchangeSession -Verifiable
+                    Mock -CommandName Test-ExtendedProtectionSPNList -Verifiable -MockWith { return $false }
+
+                    Set-TargetResource @setTargetResourceParams | Should -Throw -ExpectedMessage 'SPN list contains DotlesSPN, but AllowDotlessSPN is not added to ExtendedProtectionFlags or invalid combination was used!'
+                }
+            }
+        }
+
+        Describe 'MSFT_xExchEcpVirtualDirectory\Test-TargetResource' -Tag 'Test' {
+            AfterEach {
+                Assert-VerifiableMock
+            }
+
+
+            function Set-ECPVirtualDirectory {
+            }
+
+            $TestTargetResourceParams = @{
+                Identity   = 'EcpVirtualDirectory'
+                Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'fakeuser', (New-Object -TypeName System.Security.SecureString)
+            }
+
+            $getEcpVirtualDirectoryStandardOutput = @{
+                AdminEnabled                  = [System.Boolean] $false
+                AdfsAuthentication            = [System.Boolean] $false
+                BasicAuthentication           = [System.Boolean] $false
+                DigestAuthentication          = [System.Boolean] $false
+                ExtendedProtectionFlags         = [System.String[]] @()
+                ExtendedProtectionSPNList       = [System.String[]] @()
+                ExtendedProtectionTokenChecking = [System.String] ''
+                ExternalAuthenticationMethods = [System.String[]] @()
+                ExternalUrl                   = [System.String] ''
+                FormsAuthentication           = [System.Boolean] $false
+                GzipLevel                     = [System.String] ''
+                InternalUrl                   = [System.String] ''
+                WindowsAuthentication         = [System.Boolean] $false
+                OwaOptionsEnabled               = [System.Boolean] $false
+            }
+
+            Context 'When Test-TargetResource is called' {
+                Mock -CommandName Write-FunctionEntry -Verifiable
+                    Mock -CommandName Get-RemoteExchangeSession -Verifiable
+                It 'Should return False when Get-ECPVirtualDirectory returns False' {
+                    Mock -CommandName Get-EcpVirtualDirectoryInternal  -Verifiable
+
+                    Test-TargetResource @TestTargetResourceParams -ErrorAction SilentlyContinue | Should -Be $false
+                }
+                It 'Should return False when Test-ExchangeSetting returns False' {
+                    Mock -CommandName Get-EcpVirtualDirectoryInternal  -Verifiable -MockWith { return $getEcpVirtualDirectoryStandardOutput }
+                    Mock -CommandName Test-ExchangeSetting -Verifiable -MockWith { return $false }
+
+                    Test-TargetResource @TestTargetResourceParams | Should -Be $false
+                }
+
+                It 'Should return True when Test-ExchangeSetting returns True' {
+                    Mock -CommandName Get-EcpVirtualDirectoryInternal -Verifiable -MockWith { return $getEcpVirtualDirectoryStandardOutput }
+                    Mock -CommandName Test-ExchangeSetting -Verifiable -MockWith { return $true }
+
+                    Test-TargetResource @TestTargetResourceParams | Should -Be $true
+                }
             }
         }
     }
