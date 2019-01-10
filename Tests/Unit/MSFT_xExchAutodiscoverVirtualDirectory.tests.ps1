eb375a41426360b357281ac2317e337211055d72
Ariel Moisis <armoisis@microsoft.com>
2019-01-10 12:00:15 +1100

    fix autodiscover unit tests

--- Tests/Unit/MSFT_xExchAutodiscoverVirtualDirectory.tests.ps1
+++ Tests/Unit/MSFT_xExchAutodiscoverVirtualDirectory.tests.ps1
@@ -37,33 +37,35 @@ try
     Invoke-TestSetup
 
     InModuleScope $script:DSCResourceName {
+        Mock -CommandName Write-FunctionEntry -Verifiable
+
+        $commonTargetResourceParams = @{
+            Identity   = 'AutodiscoverVirtualDirectory'
+            Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'fakeuser', (New-Object -TypeName System.Security.SecureString)
+        }
+
+        $commonAutodiscoverVirtualDirectoryInternalStandardOutput = @{
+            BasicAuthentication             = [System.Boolean] $false
+            DigestAuthentication            = [System.Boolean] $false
+            ExtendedProtectionFlags         = [System.String[]] @()
+            ExtendedProtectionSPNList       = [System.String[]] @()
+            ExtendedProtectionTokenChecking = [System.String] ''
+            OAuthAuthentication             = [System.Boolean] $false
+            WindowsAuthentication           = [System.Boolean] $false
+            WSSecurityAuthentication        = [System.Boolean] $false
+        }
+
         Describe 'MSFT_xExchAutodiscoverVirtualDirectory\Get-TargetResource' -Tag 'Get' {
             AfterEach {
                 Assert-VerifiableMock
             }
 
-            $getTargetResourceParams = @{
-                Identity   = 'AutodiscoverVirtualDirectory'
-                Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'fakeuser', (New-Object -TypeName System.Security.SecureString)
-            }
-
-            $getAutodiscoverVirtualDirectoryInternalStandardOutput = @{
-                BasicAuthentication             = [System.Boolean] $false
-                DigestAuthentication            = [System.Boolean] $false
-                ExtendedProtectionFlags         = [System.String[]] @()
-                ExtendedProtectionSPNList       = [System.String[]] @()
-                ExtendedProtectionTokenChecking = [System.String] ''
-                OAuthAuthentication             = [System.Boolean] $false
-                WindowsAuthentication           = [System.Boolean] $false
-                WSSecurityAuthentication        = [System.Boolean] $false
-            }
-
             Context 'When Get-TargetResource is called' {
-                Mock -CommandName Write-FunctionEntry -Verifiable
-                Mock -CommandName Get-RemoteExchangeSession -Verifiable
-                Mock -CommandName Get-AutodiscoverVirtualDirectoryInternal -Verifiable -MockWith { return $getAutodiscoverVirtualDirectoryInternalStandardOutput }
 
-                Test-CommonGetTargetResourceFunctionality -GetTargetResourceParams $getTargetResourceParams
+                Mock -CommandName Get-RemoteExchangeSession -Verifiable
+                Mock -CommandName Get-AutodiscoverVirtualDirectoryInternal -Verifiable -MockWith { return $commonAutodiscoverVirtualDirectoryInternalStandardOutput }
+
+                Test-CommonGetTargetResourceFunctionality -GetTargetResourceParams $commonTargetResourceParams
             }
         }
 
@@ -72,51 +74,59 @@ try
                 Assert-VerifiableMock
             }
 
-            $setTargetResourceParams = @{
-                Identity   = 'AutodiscoverVirtualDirectory'
-                Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'fakeuser', (New-Object -TypeName System.Security.SecureString)
-            }
-
-            $getAutodiscoverVirtualDirectoryInternalStandardOutput = @{
-                BasicAuthentication             = [System.Boolean] $false
-                DigestAuthentication            = [System.Boolean] $false
-                ExtendedProtectionFlags         = [System.String[]] @()
-                ExtendedProtectionSPNList       = [System.String[]] @()
-                ExtendedProtectionTokenChecking = [System.String] ''
-                OAuthAuthentication             = [System.Boolean] $false
-                WindowsAuthentication           = [System.Boolean] $false
-                WSSecurityAuthentication        = [System.Boolean] $false
-            }
-
             Context 'When Set-TargetResource is called' {
+                function Set-AutodiscoverVirtualDirectory{
+                }
+
+                Mock -CommandName Get-RemoteExchangeSession -Verifiable
                 It 'Should warn about restarting the MSExchangeAutodiscoverAppPool' {
-                    function Set-AutodiscoverVirtualDirectory{
-                    }
-                    Mock -CommandName Write-FunctionEntry -Verifiable
-                    Mock -CommandName Get-RemoteExchangeSession -Verifiable
                     Mock -CommandName Set-AutodiscoverVirtualDirectory -Verifiable
                     Mock -CommandName Write-Warning -ParameterFilter {$message -eq 'The configuration will not take effect until MSExchangeAutodiscoverAppPool is manually recycled.'}
-                    Set-TargetResource @setTargetResourceParams
+                    Set-TargetResource @commonTargetResourceParams
                 }
 
                 It 'Should call expected functions' {
-                    function Set-AutodiscoverVirtualDirectory{
-                    }
-                    $setTargetResourceParams.AllowServiceRestart = $true
-                    Mock -CommandName Write-FunctionEntry -Verifiable
-                    Mock -CommandName Get-RemoteExchangeSession -Verifiable
+                    $commonTargetResourceParams.AllowServiceRestart = $true
                     Mock -CommandName Set-AutodiscoverVirtualDirectory -Verifiable
                     Mock -CommandName Restart-ExistingAppPool -Verifiable
-                    Set-TargetResource @setTargetResourceParams
-                    $setTargetResourceParams.Remove('AllowServiceReset')
+                    Set-TargetResource @commonTargetResourceParams
+                    $commonTargetResourceParams.Remove('AllowServiceReset')
                 }
 
                 It 'Should throw error about SPN' {
-                    Mock -CommandName Write-FunctionEntry -Verifiable
-                    Mock -CommandName Get-RemoteExchangeSession -Verifiable
                     Mock -CommandName Test-ExtendedProtectionSPNList -Verifiable -MockWith { return $false }
 
-                    Set-TargetResource @setTargetResourceParams | Should -Throw -ExpectedMessage 'SPN list contains DotlesSPN, but AllowDotlessSPN is not added to ExtendedProtectionFlags or invalid combination was used!'
+                    { Set-TargetResource @commonTargetResourceParams } | Should -Throw -ExpectedMessage 'SPN list contains DotlessSPN, but AllowDotlessSPN is not added to ExtendedProtectionFlags or invalid combination was used!'
+                }
+            }
+        }
+
+        Describe 'MSFT_xExchAutodiscoverVirtualDirectory\Test-TargetResource' -Tag 'Test' {
+            # Override Exchange cmdlets
+            AfterEach {
+                Assert-VerifiableMock
+            }
+
+            Context 'When Test-TargetResource is called' {
+                Mock -CommandName Get-RemoteExchangeSession -Verifiable
+                It 'Should return False when Get-UMCallRouterSettings returns null' {
+                    Mock -CommandName Get-AutodiscoverVirtualDirectoryInternal -Verifiable
+
+                    Test-TargetResource @commonTargetResourceParams -ErrorAction SilentlyContinue | Should -Be $false
+                }
+
+                It 'Should return False when Test-ExchangeSetting returns False' {
+                    Mock -CommandName Get-AutodiscoverVirtualDirectoryInternal -Verifiable -MockWith { return $commonAutodiscoverVirtualDirectoryInternalStandardOutput }
+                    Mock -CommandName Test-ExchangeSetting -Verifiable -MockWith { return $false }
+
+                    Test-TargetResource @commonTargetResourceParams | Should -Be $false
+                }
+
+                It 'Should return True when Test-ExchangeSetting returns True' {
+                    Mock -CommandName Get-AutodiscoverVirtualDirectoryInternal -Verifiable -MockWith { return $commonAutodiscoverVirtualDirectoryInternalStandardOutput }
+                    Mock -CommandName Test-ExchangeSetting -Verifiable -MockWith { return $true }
+
+                    Test-TargetResource @commonTargetResourceParams | Should -Be $true
                 }
             }
         }
