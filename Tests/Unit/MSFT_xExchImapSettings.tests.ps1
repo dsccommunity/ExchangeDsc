9236a3df37a8e5b71b16fa3f412ad5295fac7441
Ariel Moisis <armoisis@microsoft.com>
2019-01-10 11:18:35 +1100

    add missing params

--- Tests/Unit/MSFT_xExchImapSettings.tests.ps1
+++ Tests/Unit/MSFT_xExchImapSettings.tests.ps1
@@ -37,28 +37,54 @@ try
     Invoke-TestSetup
 
     InModuleScope $script:DSCResourceName {
+
+        Mock -CommandName Write-FunctionEntry -Verifiable
+
+        $commonTargetResourceParams = @{
+            Server     = 'Server'
+            Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'fakeuser', (New-Object -TypeName System.Security.SecureString)
+        }
+
+        $commonImapSettingsStandardOutput = @{
+            ExternalConnectionSettings        = [System.String[]] @()
+            LoginType                         = [System.String] ''
+            X509CertificateName               = [System.String] ''
+            AuthenticatedConnectionTimeout    = [System.String] ''
+            Banner                            = [System.String] ''
+            CalendarItemRetrievalOption       = [System.String] ''
+            EnableExactRFC822Size             = [System.Boolean] $false
+            EnableGSSAPIAndNTLMAuth           = [System.Boolean] $false
+            EnforceCertificateErrors          = [System.Boolean] $false
+            ExtendedProtectionPolicy          = [System.String] ''
+            InternalConnectionSettings        = [System.String[]] @()
+            LogFileLocation                   = [System.String] ''
+            LogFileRollOverSettings           = [System.String] ''
+            LogPerFileSizeQuota               = [System.String[]] @()
+            MaxCommandSize                    = [System.Int32] 1
+            MaxConnectionFromSingleIP         = [System.Int32] 1
+            MaxConnections                    = [System.Int32] 1
+            MaxConnectionsPerUser             = [System.Int32] 1
+            MessageRetrievalMimeFormat        = [System.String] ''
+            OwaServerUrl                      = [System.String] ''
+            PreAuthenticatedConnectionTimeout = [System.String] ''
+            ProtocolLogEnabled                = [System.Boolean] $false
+            ProxyTargetPort                   = [System.Int32] 1
+            ShowHiddenFoldersEnabled          = [System.Boolean] $false
+            SSLBindings                       = [System.String[]] @()
+            SuppressReadReceipt               = [System.Boolean] $false
+            UnencryptedOrTLSBindings          = [System.String[]] @()
+        }
         Describe 'MSFT_xExchImapSettings\Get-TargetResource' -Tag 'Get' {
             AfterEach {
                 Assert-VerifiableMock
             }
 
-            $getTargetResourceParams = @{
-                Server     = 'Server'
-                Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'fakeuser', (New-Object -TypeName System.Security.SecureString)
-            }
-
-            $getImapSettingsStandardOutput = @{
-                ExternalConnectionSettings = [System.String[]] @()
-                LoginType                  = [System.String] ''
-                X509CertificateName        = [System.String] ''
-            }
-
             Context 'When Get-TargetResource is called' {
-                Mock -CommandName Write-FunctionEntry -Verifiable
-                Mock -CommandName Get-RemoteExchangeSession -Verifiable
-                Mock -CommandName Get-ImapSettingsInternal -Verifiable -MockWith { return $getImapSettingsStandardOutput }
 
-                Test-CommonGetTargetResourceFunctionality -GetTargetResourceParams $getTargetResourceParams
+                Mock -CommandName Get-RemoteExchangeSession -Verifiable
+                Mock -CommandName Get-ImapSettingsInternal -Verifiable -MockWith { return $commonImapSettingsStandardOutput }
+
+                Test-CommonGetTargetResourceFunctionality -GetTargetResourceParams $commonTargetResourceParams
             }
         }
     }
